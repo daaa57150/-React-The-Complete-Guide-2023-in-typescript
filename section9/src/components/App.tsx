52903d881fb778231a41dfa33e3ad5fafe28e049
David Kern <dkern@agilepartner.net>
2023-11-22 11:46:05 +0100

    Section 9 - 166: Converted project to typescript

--- section9/src/components/App.tsx
+++ section9/src/components/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import React from 'react';
+
+export default function App() {
+
+  console.log('Running App :)');
+
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
-
-export default App;
