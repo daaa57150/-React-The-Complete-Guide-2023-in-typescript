97ded69daed8ea80da4bd7ec555c522906bf18c3
David Kern <dkern@agilepartner.net>
2023-12-12 18:02:57 +0100

    Section 15 - 297: Added fetch

--- section15/src/components/AvailablePlaces.tsx
+++ section15/src/components/AvailablePlaces.tsx
@@ -1,4 +1,5 @@
 import { Place } from '@models/place.model.js';
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 interface Props {
@@ -6,10 +7,24 @@ interface Props {
 }
 
 export default function AvailablePlaces({ onSelectPlace }: Props) {
+
+  const [availablePlaces, setAvailablePlaces] = useState<Place[]>([]);
+
+  async function fetchPlaces() {
+    const response = await fetch("http://localhost:3000/places");
+    const data = await response.json();
+    setAvailablePlaces(data.places);
+  }
+
+  useEffect(() => {
+    fetchPlaces()
+  }, []);
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- section15/src/components/Places.tsx
+++ section15/src/components/Places.tsx
@@ -1,30 +1,41 @@
 import { Place } from "@models/place.model";
+import { Case, Default, Switch } from "react-if";
 
 interface Props {
   title: string;
   places: Place[];
   fallbackText: string;
   onSelectPlace: (place: Place) => void;
+  isLoading: boolean;
+  loadingText: string;
 }
 
-export default function Places({ title, places, fallbackText, onSelectPlace }: Props) {
+export default function Places({ title, places, fallbackText, onSelectPlace, isLoading, loadingText }: Props) {
   console.log(places);
+
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
-        <ul className="places">
-          {places.map((place) => (
-            <li key={place.id} className="place-item">
-              <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
-                <h3>{place.title}</h3>
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+      <Switch>
+        <Case condition={isLoading}>
+          <p className="fallback-text">{loadingText}</p>
+        </Case>
+        <Case condition={places.length === 0}>
+          <p className="fallback-text">{fallbackText}</p>
+        </Case>
+        <Default>
+          <ul className="places">
+            {places.map((place) => (
+              <li key={place.id} className="place-item">
+                <button onClick={() => onSelectPlace(place)}>
+                  <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                  <h3>{place.title}</h3>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Default>
+      </Switch>
     </section>
   );
 }
